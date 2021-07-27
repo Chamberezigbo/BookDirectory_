026c8b12c2305656805298994032e9a51b27f7c2
chamberezigbo <chamberezigbo@gmail.com>
2021-07-27 17:13:17 +0100

    restructured create rout

--- src/router/user.js
+++ src/router/user.js
@@ -1,70 +1,101 @@
-const express = require('express');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const auth = require("../middleware/auth");
+const User = require("../models/user");
+const router = new express.Router();
 
-const auth = require('../middleware/auth')
-const User = require('../models/user');
-const router = new express.Router()
+// router.post('/users', async (req, res) => {
+//        const user = new User(req.body)
+//      if(await User.findOne({email:req.body.email}))res.status(400).send('email already existing')
 
-router.post('/users', async (req, res) => {
-       const user = new User(req.body)
-     if(await User.findOne({email:req.body.email}))res.status(400).send('email already existing')
+//        try {
+//               await user.save()
+//               const token = await user.generateAuthToken()
+//               res.status(201).send({ user, token })
+//        } catch (e) {
+//               res.status(400).send(e.message)
+//        }
+// })
+
+router.post("/users", async (request, response) => {
+
 
        try {
-              await user.save()
-              const token = await user.generateAuthToken()
-              res.status(201).send({ user, token })
-       } catch (e) {
-              res.status(400).send(e)
+              let { firstName, lastName, email, password, gender } = request.body;
+              password = bcrypt.hashSync(request.body.password, 10);
+            
+              let existUser = await User.findOne({ email: request.body.email });
+              if (existUser)
+                return response
+                  .status(401)
+                  .json({
+                    success: false,
+                    responseMessage: `User already exist with this email ${existUser.email}`,
+                  });
+            
+                  let newUser = new User({
+                         firstName, lastName,email,password,gender
+                  })
+                  let userData =  await newUser.save
+                  return response.status(200).json({success:true, responseMessage:userData}) 
+       } catch (error) {
+              return response.status(422).json({success:false , responseMessage:`Failed to register user due to ${error}`})
        }
-})
+ 
+});
 
-router.post('/users/login', async (req,res) => {
-       try {
-              const user = await User.findByCredentials(req.body.email,req.body.password)
-              const token = await user.generateAuthToken()
-              res.send({user, token})  
-       } catch (e) {
-              res.status(400).send()
-       }
-})
+router.post("/users/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (e) {
+    res.status(400).send();
+  }
+});
 
-router.post('/users/logout', auth, async (req, res) => {
-       try {
-              req.user.tokens = req.user.tokens.filter((token) => {
-                  return token.token !== req.token
-              })
-              await req.user.save()
-      
-              res.send()
-       } catch (e) {
-              res.status(500).send()
-       }
-})
+router.post("/users/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
 
-router.post('/users/logoutall', auth, async (req,res) => {
-       try {
-              req.user.tokens = []
-              await req.user.save()
-              res.send()
-       } catch (e) {
-              res.status(500).send()
-       }
-})
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
 
-router.patch('/users/me', auth, async (req,res) => {
-       const updates = Object.keys(req.body);
-       const allowedUpdates = ['name', 'email', 'password', 'gender'];
-       const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+router.post("/users/logoutall", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
 
-       if (!isValidOperation)res.status(400).send({error: 'invalide operation'})
-       
-       try {
-              updates.forEach((update) => req.user[update] = req.body[update])
-              await req.user.save()
-              res.send(req.user)
-       } catch (e) {
-              res.status(400).send(e)
-       }
-       
-})
+router.patch("/users/me", auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password", "gender"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
 
-module.exports = router
+  if (!isValidOperation) res.status(400).send({ error: "invalide operation" });
+
+  try {
+    updates.forEach((update) => (req.user[update] = req.body[update]));
+    await req.user.save();
+    res.send(req.user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+module.exports = router;
